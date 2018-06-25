7d9c78b03a9a0e8e2eaac229a1cea57ddd727561
Osvaldo Vargas <osvaldo@ncite.mx>
2018-06-25 10:47:29 -0500

    Movement inprove
    Change the movement logic

--- 3D Platformer/Assets/Scripts/PlayerController.cs
+++ 3D Platformer/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour {
     public float jumpForce;
     public float movementSpeed;
     Rigidbody rb;
+    LayerMask ground;
 
     public float gravityScale;
 
@@ -19,38 +20,47 @@ public class PlayerController : MonoBehaviour {
     bool jumping;
     // Use this for initialization
     void Start () {
-        controller = GetComponent<CharacterController>();
-        //rb = GetComponent<Rigidbody>();
+        //controller = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
         jumping = false;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate ()
     {
+
+        if(Input.GetAxisRaw("Vertical")!=0f || Input.GetAxisRaw("Horizontal") != 0f)
+        {
+
+            movementVector = (transform.forward * Input.GetAxisRaw("Vertical") + transform.right * Input.GetAxisRaw("Horizontal")).normalized * movementSpeed;
+
+        }else
+        {
+            print("lol");
+            // float oldDrag = rb.drag;
+            //float oldMass = rb.mass;
+
+            //rb.drag = 2000;
+            //rb.mass = 5;
+            movementVector = new Vector3(0f, movementVector.y, 0f);
+
+            //rb.drag = oldDrag;
+            //rb.mass = oldMass;
+        }
+
+        movementVector.y = movementVector.y + (Physics.gravity.y * gravityScale);
+        rb.velocity = movementVector * Time.fixedDeltaTime;
+
+
         /*
-        //make sure you are reading something
-        if(!(Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0))
-        {
-            
-        }
-        movementVector = transform.forward * Input.GetAxis("Vertical") + transform.right * Input.GetAxis("Horizontal");
-        rb.velocity = (movementVector.normalized * movementSpeed ) + (new Vector3(0, rb.velocity.y, 0));
-
-
-        if (rb.velocity.y == 0 && jumping)
-        {
-            jumping = false;
-        }
-
         if (Input.GetKeyDown(KeyCode.Space) && !jumping)
         {
             //rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             jumping = true;
         }*/
-        movementVector = (transform.forward * Input.GetAxis("Vertical") + transform.right * Input.GetAxis("Horizontal")).normalized * movementSpeed + new Vector3( 0f,movementVector.y ,0f);
 
-
+        /*
         if ( controller.isGrounded)
         {
             movementVector.y = 0f;
@@ -60,15 +70,15 @@ public class PlayerController : MonoBehaviour {
             {
                 movementVector.y = jumpForce;
             }
-        }
-        
+        }*/
 
-        movementVector.y = movementVector.y + (Physics.gravity.y * gravityScale);
-        controller.Move(movementVector* Time.fixedDeltaTime);
+
+        //movementVector.y = movementVector.y + (Physics.gravity.y * gravityScale);
+        //controller.Move(movementVector* Time.smoothDeltaTime);
     }
 
     public bool IsGrounded()
     {
-        return controller.isGrounded;
+        return true;
     }
 }
